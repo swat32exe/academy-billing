852fc532a11abdb6cfa5d685b7512b85f2a50819
Тодер Святослав <toderswat@gmail.com>
2013-07-06 12:35:34 +0300

    Blocks implementation modified.

--- AcademyBilling/AcademyBilling/DefaultBillingRules.cpp
+++ AcademyBilling/AcademyBilling/DefaultBillingRules.cpp
@@ -14,51 +14,22 @@ namespace AcademyBilling
     {
     }
 
-    int DefaultBillingRules::chargeForCall(Subscriber &subscriber, const Call &call)
+    int DefaultBillingRules::chargeForCall(const Call &call, Subscriber &subscriber)
     {
-        
-        int callDuration = call.getDuration();
-        time_t callTime = call.getTime();
-        std::string number = call.getCalleeNumber();
-
-        // Free minutes will be modified implicitly.
-        AcademyBilling::DefaultBillingRuleBlockCallInfo callInfo(callDuration, callTime, lastCreditTime, freeMinutesSinceLastCredit, number);
-        
-        int callCost = defaultBillingRulesRuleset.calculateCallCost(callInfo);
-        subscriber.charge(callCost);
-
-        return callCost;
+        return defaultBillingRulesRuleset.chargeForCall(call, subscriber);
     }
 
-    int DefaultBillingRulesRuleset::calculateCallCost(DefaultBillingRuleBlockCallInfo &callInfo)
+    int DefaultBillingRulesRuleset::chargeForCall(Call call, Subscriber &subscriber)
     {
-        return firstRuleBlock->calculateCallCost(callInfo);
+        return firstRuleBlock->chargeForCall(call,subscriber);
     }
 
     DefaultBillingRulesRuleset::DefaultBillingRulesRuleset()
     {
-        std::vector<std::string> insideNetworkPrefixes;
-        insideNetworkPrefixes.push_back("050");
-        insideNetworkPrefixes.push_back("066");
-        insideNetworkPrefixes.push_back("095");
-        insideNetworkPrefixes.push_back("099");
-
-        // Last block in sequence.
-        std::auto_ptr<DefaultBillingRuleBlock> stop(new DefaultBillingRuleBlockStop());
-
-        // Charge fixed fee 0.95 and stop processing.
-        std::auto_ptr<DefaultBillingRuleBlock> fixedMinuteFeeOutsideNetwork(new DefaultBillingRuleBlockChargeFixedMinuteFee(95, stop));
-
-        // Charge fixed fee 0.50 and stop processing.
-        std::auto_ptr<DefaultBillingRuleBlock> fixedMinuteFeeInsideNetwork(new DefaultBillingRuleBlockChargeFixedMinuteFee(50, stop));
-        // Use free minutes, that were provided after last refill. If not enough go to fixedMinuteFeeInsideNetwork rule.
-        std::auto_ptr<DefaultBillingRuleBlock> freeMinutesAfterCredit(new DefaultBillingRuleBlockFreeMinutesAfterCredit(30 * Utility::secondsInDay, fixedMinuteFeeInsideNetwork));
-        // Use 5 free minutes, if it's weekend.
-        std::auto_ptr<DefaultBillingRuleBlock> weekendFreeMinutes(new DefaultBillingRuleBlockWeekendFreeMinutes(5,freeMinutesAfterCredit));
-
-        // Select rules according ro called subscriber network.
-        std::auto_ptr<DefaultBillingRuleBlock> networkTypeSplitter(new DefaultBillingRuleBlockNetworkTypeSplitter(insideNetworkPrefixes,weekendFreeMinutes,fixedMinuteFeeOutsideNetwork));
-        // Fixed connection fee.
-        firstRuleBlock =  std::auto_ptr<DefaultBillingRuleBlock>(new DefaultBillingRuleBlockConnectionFee(33,networkTypeSplitter));
+        firstRuleBlock =  std::auto_ptr<DefaultBillingRuleBlock>(
+                            new DefaultBillingRuleBlockConnectionFee(
+                            new DefaultBillingRuleBlockWeekendFreeMinutes(
+                            new DefaultBillingRuleBlockFreeMinutesAfterCredit(
+                            new DefaultBillingRuleBlockChargeFixedMinuteFee(NULL)))));
     }
 }
