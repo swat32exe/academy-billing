ebd5104cd6cfe977859063fbadf18e42629e4327
Polart23 <artempolianskiy@yandex.ru>
2013-07-07 00:11:45 +0300

    add getters in Subscribers.
    Replace tabs.

--- AcademyBilling/AcademyBilling/Subscriber.cpp
+++ AcademyBilling/AcademyBilling/Subscriber.cpp
@@ -4,65 +4,81 @@
 
 namespace AcademyBilling
 {
-	const std::string Subscriber::mask="+3(***)*******";
+    const std::string Subscriber::mask="+3(***)*******";
 
-	Subscriber::Subscriber(const std::string& _number, const int& _balance, BillingRules* _tariff, const Refill _lastRefill)
-		:lastRefill(_lastRefill)
-	{
-		setNumber(_number);
-		balance=_balance;
-		tariff=_tariff;
-	}
+    Subscriber::Subscriber(const std::string& _number, const int& _balance, BillingRules* _tariff, const Refill _lastRefill)
+        :lastRefill(_lastRefill)
+    {
+        setNumber(_number);
+        balance=_balance;
+        tariff=_tariff;
+    }
 
-	Subscriber::~Subscriber(void)
-	{
-	}
+    Subscriber::~Subscriber(void)
+    {
+    }
 
-	int Subscriber::getBalance() const
-	{
-		return balance;
-	}
+    int Subscriber::getBalance() const
+    {
+        return balance;
+    }
 
-	std::string Subscriber::getNumber() const
+    std::string Subscriber::getNumber() const
+    {
+        return number;
+    }
+
+	BillingRules* Subscriber::getTariff() const
 	{
-		return number;
-	}
-	void Subscriber::setNumber(const std::string& str)
-	{
-		if(isNumberValid(str))
-		{
-			number=str;
-		}
-		else
-		{
-			throw std::invalid_argument("Invalid number");			
-		}
+		return tariff;
 	}
 
-	int Subscriber::charge(const int& sum)
+	Refill Subscriber::getLastRefill() const
 	{
-		return balance-=sum;
+		return lastRefill;
 	}
 
-	int Subscriber::addRefill(const Refill& refill)
+	time_t Subscriber::getLastRefillTime() const
 	{
-		lastRefill=refill;
-		return balance+=refill.getMoney();
+		return lastRefill.getTime();
 	}
 
-	bool Subscriber::isNumberValid(const std::string& str)
-	{
-		if (str.length()!=mask.length())
-		{
-			return false;
-		}
-		for (size_t i=0;i<mask.length();++i)
-		{
-			if (!(mask[i]=='*' && isdigit(str[i]) || mask[i]!='*' && mask[i]==str[i]))
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+    void Subscriber::setNumber(const std::string& str)
+    {
+        if(isNumberValid(str))
+        {
+            number=str;
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid number");            
+        }
+    }
+
+    int Subscriber::charge(const int& sum)
+    {
+        return balance-=sum;
+    }
+
+    int Subscriber::addRefill(const Refill& refill)
+    {
+        lastRefill=refill;
+        return balance+=refill.getMoney();
+    }
+
+    bool Subscriber::isNumberValid(const std::string& str)
+    {
+        if (str.length()!=mask.length())
+        {
+            return false;
+        }
+        for (size_t i=0;i<mask.length();++i)
+        {
+            if (!(mask[i]=='*' && isdigit(str[i]) || mask[i]!='*' && mask[i]==str[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- AcademyBilling/AcademyBilling/Subscriber.h
+++ AcademyBilling/AcademyBilling/Subscriber.h
@@ -9,25 +9,28 @@
 
 namespace AcademyBilling
 {
-	class Subscriber
-	{
-	private: 
-		int balance;
-		std::string number;
-		BillingRules *tariff;
-		Refill lastRefill;
-		const static std::string mask;
-	public:
-		Subscriber(const std::string&, const int&, BillingRules*, const Refill);
-		Subscriber(const Subscriber&);
-		~Subscriber();
-		int getBalance() const;
-		std::string getNumber() const;
-		void setNumber(const std::string&);
-		int charge(const int&);
-		int addRefill(const Refill&);
-		static bool isNumberValid(const std::string&);
-	};
+    class Subscriber
+    {
+    private: 
+        int balance;
+        std::string number;
+        BillingRules *tariff;
+        Refill lastRefill;
+        const static std::string mask;
+    public:
+        Subscriber(const std::string&, const int&, BillingRules*, const Refill);
+        Subscriber(const Subscriber&);
+        ~Subscriber();
+        int getBalance() const;
+        std::string getNumber() const;
+        BillingRules* getTariff() const;
+        Refill getLastRefill() const;
+        time_t getLastRefillTime() const;
+        void setNumber(const std::string&);
+        int charge(const int&);
+        int addRefill(const Refill&);
+        static bool isNumberValid(const std::string&);
+    };
 }
 
 #endif
